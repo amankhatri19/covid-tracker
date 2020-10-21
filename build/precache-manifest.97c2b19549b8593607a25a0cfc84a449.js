d14b8332814831d25f68cf3fdb3e38d3fc209f08
amankhatri19 <61044807+amankhatri19@users.noreply.github.com>
2020-10-21 15:41:07 +0530

    Add files via upload

---
+++ build/precache-manifest.97c2b19549b8593607a25a0cfc84a449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f03dd11806c16e9676be9093b2dc929",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3a2688e6bb41d75a578",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "8e3190220136f5d450ec",
+    "url": "/static/css/main.45690d15.chunk.css"
+  },
+  {
+    "revision": "f3a2688e6bb41d75a578",
+    "url": "/static/js/2.4383afd9.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.4383afd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e3190220136f5d450ec",
+    "url": "/static/js/main.bb4a6be6.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
